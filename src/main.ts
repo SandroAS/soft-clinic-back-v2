72699a8acafbe9711b5dbea7d2ac889d2043a349
SandroGreenn <sandro@greenn.com.br>
2025-05-31 10:18:33 -0300

    validation pipie e prefix api e cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:8081'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
