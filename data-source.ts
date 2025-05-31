32a5f7b812a490db9e548169a0ecc7c6a459e2b2
SandroGreenn <sandro@greenn.com.br>
2025-05-31 19:49:33 -0300

    data source ajustado

--- data-source.ts
+++ data-source.ts
@@ -3,14 +3,14 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export default new DataSource({
+export const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [__dirname + '/entities/*.entity.ts'],
-  migrations: [__dirname + '/migrations/*.ts'],
+  entities: ['src/entities/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
   synchronize: false,
 });
