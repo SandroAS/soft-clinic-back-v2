03885bc57755c66b134affee09703882ade92937
SandroGreenn <sandro@greenn.com.br>
2025-05-31 10:40:40 -0300

    importando no app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_PIPE } from '@nestjs/core';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 const cookieSession = require('cookie-session');
 
@@ -26,6 +28,8 @@ const cookieSession = require('cookie-session');
         synchronize: true,
       }),
     }),
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [
