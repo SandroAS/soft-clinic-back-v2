77d16209510b0c935c41a4fd4511a270d796c78f
SandroGreenn <sandro@greenn.com.br>
2025-06-12 19:28:09 -0300

    seeders system modules

--- src/seeds/run-seeders.ts
+++ src/seeds/run-seeders.ts
@@ -1,9 +1,11 @@
 import { seedPermissionsRoles } from './seed-permissions-roles';
 import { seedPlans } from './seed-plans';
+import { seedSystemModules } from './seed-system-modules';
 
 export async function runSeeders() {
   console.log('Running seeders...');
   await seedPermissionsRoles();
   await seedPlans();
+  await seedSystemModules();
   console.log('Seeders finished.');
 }
---
+++ src/seeds/seed-system-modules.ts
@@ -0,0 +1,29 @@
+import AppDataSource from '../data-source';
+import { SystemModule, SystemModuleName } from '../entities/system-module.entity';
+
+export async function seedSystemModules() {
+  const systemModules = [
+    { name: SystemModuleName.DENTISTRY },
+    { name: SystemModuleName.PSYCHOLOGY },
+    { name: SystemModuleName.NUTRITION },
+    { name: SystemModuleName.PHYSIOTHERAPY },
+  ];
+
+  const systemModuleRepo = AppDataSource.getRepository(SystemModule);
+
+  for (const moduleData of systemModules) {
+
+    const exists = await systemModuleRepo.findOneBy({ name: moduleData.name });
+
+    if (!exists) {
+      // Se não existe, cria e salva o novo módulo
+      const module = systemModuleRepo.create(moduleData);
+      await systemModuleRepo.save(module);
+      console.log(`✅ Módulo de sistema "${moduleData.name}" inserido com sucesso.`);
+    } else {
+      console.log(`❕ Módulo de sistema "${moduleData.name}" já existe. Pulando inserção.`);
+    }
+  }
+
+  console.log('✨ Seed de Módulos de Sistema concluído!');
+}
